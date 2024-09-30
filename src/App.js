2ab1c5f318e86259eb041e63bc31f7fe75a50fc8
tlsgnvkr <shinhup4743@gmail.com>
2024-10-01 01:51:59 +0900

    3주차 실습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import dummyData from './dummyData.js' 
+import './App.css'
+import React from 'react'
+import titleImage from './title.png'
 
-function App() {
+function Image() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <img src={titleImage} width='100%' alt='pokemon page title'/>
+  );
+}
+
+function InnerItems(props) {
+  const outputData = dummyData[Number(props.num)];
+  
+  return (
+    <div>
+      <div class="title"><h1>{outputData.title}</h1></div>
+      <div class="content"><p>{outputData.content}</p></div>
+      <div class="type"><h4>{outputData.type}</h4></div>
     </div>
   );
 }
 
-export default App;
+function MainTable() {
+  return (
+    <div class="parent">
+      <InnerItems num='0'/>
+      <InnerItems num='1'/>
+      <InnerItems num='2'/>
+      <InnerItems num='3'/>
+      <InnerItems num='4'/>
+      <InnerItems num='5'/>
+      <InnerItems num='6'/>
+      <InnerItems num='7'/>
+      <InnerItems num='8'/>
+      <InnerItems num='9'/>
+      <InnerItems num='10'/>
+      <InnerItems num='11'/>
+      <InnerItems num='12'/>
+      <InnerItems num='13'/>
+      <InnerItems num='14'/>
+      <InnerItems num='15'/>
+      <InnerItems num='16'/>
+      <InnerItems num='17'/>
+      <InnerItems num='18'/>
+      <InnerItems num='19'/>
+      <InnerItems num='20'/>
+      <InnerItems num='21'/>
+      <InnerItems num='22'/>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Image/>
+      <MainTable/>
+    </div>
+  );
+}
+
+export default App
---
+++ src/dummyData.js
@@ -0,0 +1,27 @@
+const dummyData = [
+    { title: '1', content: '1번째 포켓몬', type: '풀' },
+    { title: '2', content: '2번째 포켓몬', type: '불꽃' },
+    { title: '3', content: '3번째 포켓몬', type: '물' },
+    { title: '4', content: '4번째 포켓몬', type: '전기' },
+    { title: '5', content: '5번째 포켓몬', type: '페어리' },
+    { title: '6', content: '6번째 포켓몬', type: '강철' },
+    { title: '7', content: '7번째 포켓몬', type: '바위' },
+    { title: '8', content: '8번째 포켓몬', type: '어둠' },
+    { title: '9', content: '9번째 포켓몬', type: '격투' },
+    { title: '10', content: '10번째 포켓몬', type: '독' },
+    { title: '11', content: '11번째 포켓몬', type: '땅' },
+    { title: '12', content: '12번째 포켓몬', type: '얼음' },
+    { title: '13', content: '13번째 포켓몬', type: '빛' },
+    { title: '14', content: '14번째 포켓몬', type: '물' },
+    { title: '15', content: '15번째 포켓몬', type: '불꽃' },
+    { title: '16', content: '16번째 포켓몬', type: '전기' },
+    { title: '17', content: '17번째 포켓몬', type: '바위' },
+    { title: '18', content: '18번째 포켓몬', type: '풀' },
+    { title: '19', content: '19번째 포켓몬', type: '어둠' },
+    { title: '20', content: '20번째 포켓몬', type: '페어리' },
+    { title: '21', content: '21번째 포켓몬', type: '독' },
+    { title: '22', content: '22번째 포켓몬', type: '격투' },
+    { title: '23', content: '23번째 포켓몬', type: '빛' },
+  ];
+  
+  export default dummyData;
